b280eeb94abcfd07cb30d379b68b4af259eec6cf
usama7571 <100509779+usama7571@users.noreply.github.com>
2026-07-16 23:21:58 +0500

    Await database initialization in main function
    Initialize database before running the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'core/database/database_helper.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await DatabaseHelper.instance.database;
+
   runApp(const UFCApp());
 }
